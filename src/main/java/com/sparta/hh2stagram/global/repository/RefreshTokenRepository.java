dfae4bb4dabcf85e254589386a2f2383e7dc9603
sanha <0412csh@gmail.com>
2024-03-19 17:49:09 +0900

    feat: global 추가

---
+++ src/main/java/com/sparta/hh2stagram/global/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.hh2stagram.global.repository;
+
+import com.sparta.balance.global.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+}
