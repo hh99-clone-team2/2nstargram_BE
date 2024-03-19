ef0703e32c9ad9c2fba6a037c29cc8c1ec2d4779
minnieming <eunmilee9109.1@gmail.com>
2024-03-19 17:40:04 +0900

    init commit

---
+++ src/test/java/com/sparta/hh2stagram/Hh2stagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hh2stagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh2stagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
