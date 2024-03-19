1a2b361beaa14f3d93a374291f583371240f6632
minnieming <eunmilee9109.1@gmail.com>
2024-03-19 17:40:04 +0900

    init commit

---
+++ src/main/java/com/sparta/hh2stagram/Hh2stagramApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hh2stagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh2stagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh2stagramApplication.class, args);
+    }
+
+}
