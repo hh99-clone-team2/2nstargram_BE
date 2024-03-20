37f97bcc70966396cd0c2c5c84c50107785156f4
Seungmi97 <mi.seung97@gmail.com>
2024-03-20 14:24:43 +0900

    Feat: 회원가입, 로그인 기능 구현

--- src/main/java/com/sparta/hh2stagram/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/hh2stagram/domain/user/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
-    Optional<User> findByNickname(String nickname);
+    Optional<User> findByUsername(String username);
 }
