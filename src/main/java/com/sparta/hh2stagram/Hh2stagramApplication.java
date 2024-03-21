0afe99884dad60e1631702cfe03e4b97eae13775
minnieming <eunmilee9109.1@gmail.com>
2024-03-21 20:53:32 +0900

    #3 FEAT : createdAt 출력 성공

--- src/main/java/com/sparta/hh2stagram/Hh2stagramApplication.java
+++ src/main/java/com/sparta/hh2stagram/Hh2stagramApplication.java
@@ -2,7 +2,9 @@ package com.sparta.hh2stagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Hh2stagramApplication {
 
