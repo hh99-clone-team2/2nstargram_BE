417748fc1dc8901b679c2b984fedfbb0907305ad
Seungmi97 <mi.seung97@gmail.com>
2024-03-21 14:56:26 +0900

    Feat: 유저 검색 기능 추가

--- src/main/java/com/sparta/hh2stagram/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/hh2stagram/domain/user/controller/UserController.java
@@ -11,10 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "UserController", description = "회원 가입, 로그인 API 컨트롤러")
 @Slf4j(topic = "회원 가입, 로그인")
@@ -33,7 +32,7 @@ public class UserController {
     @Operation(summary = "회원 가입", description = "이메일 또는 휴대폰 번호, 비밀번호, 유저 이름을 등록합니다.")
     @ApiResponse(responseCode = "201", description = "회원 가입 완료")
     /*회원가입 기능 호출*/
-    ResponseEntity<Void> signupUser(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<Void> signupUser(@RequestBody SignupRequestDto requestDto) {
 
         userService.signupUser(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -43,7 +42,12 @@ public class UserController {
     @Operation(summary = "로그인", description = "회원 이메일 또는 휴대폰 번호, 비밀번호를 입력해 로그인할 수 있습니다.")
     @ApiResponse(responseCode = "200", description = "로그인 완료")
     /*로그인 기능 호출*/
-    ResponseEntity<UserResponseDto> loginUser(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
+    public ResponseEntity<UserResponseDto> loginUser(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.loginUser(requestDto, response));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<List<UserResponseDto>> getUserByUsername(@PathVariable String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserByUsername(username));
     }
 }
--- src/main/java/com/sparta/hh2stagram/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/hh2stagram/domain/user/repository/UserRepository.java
@@ -4,10 +4,13 @@ package com.sparta.hh2stagram.domain.user.repository;
 import com.sparta.hh2stagram.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
     Optional<User> findByUsername(String username);
+
+    List<User> findByUsernameContains(String username);
 }
