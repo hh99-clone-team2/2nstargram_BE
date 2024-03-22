7db5183fe7b3fa0dc0ddf264baa8383fc778e68e
minnieming <eunmilee9109.1@gmail.com>
2024-03-22 14:17:24 +0900

    Refactor : FollowController response 여러정보로 싸기

--- src/main/java/com/sparta/hh2stagram/domain/follow/controller/FollowController.java
+++ src/main/java/com/sparta/hh2stagram/domain/follow/controller/FollowController.java
@@ -5,10 +5,12 @@ import com.sparta.hh2stagram.domain.follow.entity.Follow;
 import com.sparta.hh2stagram.domain.follow.service.FollowService;
 import com.sparta.hh2stagram.domain.user.entity.User;
 import com.sparta.hh2stagram.domain.user.service.UserService;
+import com.sparta.hh2stagram.global.refreshToken.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,37 +36,37 @@ public class FollowController {
     @Operation(summary = "팔로우", description = "팔로우 추가")
     @ApiResponse(responseCode = "200", description = "팔로우 성공")
     @PostMapping("/user/follow/{userId}")
-    public ResponseEntity<String> follow(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<?> follow(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
         User fromUser = userService.findByUsername(userDetails.getUsername()); // 현재 사용자 정보 가져오기
         User toUser = userService.findById(userId); // 팔로우할 대상 사용자 정보 가져오기
         followService.follow(fromUser, toUser); // FollowService의 follow 메소드 호출
-        return ResponseEntity.ok("팔로우 되었습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success("팔로우 되었습니다.", null));
     }
 
     @Operation(summary = "언팔로우", description = "언팔로우")
     @ApiResponse(responseCode = "200", description = "언팔로우 성공")
     @DeleteMapping("/user/follow/{userId}")
-    public ResponseEntity<String> unfollow(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<?> unfollow(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
         User fromUser = userService.findByUsername(userDetails.getUsername()); // 현재 사용자 정보 가져오기
         User toUser = userService.findById(userId); // 언팔로우할 대상 사용자 정보 가져오기
         followService.unfollow(fromUser, toUser); // FollowService의 unfollow 메소드 호출
-        return ResponseEntity.ok("언팔로우 되었습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success("언팔로우 되었습니다.", null));
     }
 
     @Operation(summary = "팔로잉 목록 조회", description = "팔로잉 목록 조회")
-    @ApiResponse(responseCode = "200", description = "팔로워 목록 조회 성공")
+    @ApiResponse(responseCode = "200", description = "팔로잉 목록 조회 성공")
     @GetMapping("/user/{userId}/following")
-    public ResponseEntity<List<FollowResponseDto>> getFollowingList(@PathVariable Long userId) {
+    public ResponseEntity<?> getFollowingList(@PathVariable Long userId) {
         List<FollowResponseDto> followingList = followService.getFollowingList(userId);
-        return ResponseEntity.ok(followingList);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success("팔로잉 목록 조회 성공", followingList));
     }
 
     @Operation(summary = "팔로워 목록 조회", description = "팔로워 목록 조회")
     @ApiResponse(responseCode = "200", description = "팔로워 목록 조회 성공")
     @GetMapping("/user/{userId}/follower")
-    public ResponseEntity<List<FollowResponseDto>> getFollowerList(@PathVariable Long userId) {
+    public ResponseEntity<?> getFollowerList(@PathVariable Long userId) {
         User user = userService.findById(userId); // 조회할 사용자 정보 가져오기
         List<FollowResponseDto> followerList = followService.getFollowerList(userId); // 팔로워 목록 조회
-        return ResponseEntity.ok(followerList);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success("팔로워 목록 조회 성공", followerList));
     }
 }
