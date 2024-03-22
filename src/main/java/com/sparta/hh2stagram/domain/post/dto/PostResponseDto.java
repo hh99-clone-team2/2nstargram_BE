a2eacff7113851bef82554f0e5b94f76c1766c78
Seungmi97 <mi.seung97@gmail.com>
2024-03-22 19:04:44 +0900

    Feat: 게시물 작성, 수정, 삭제, 조회 기능 구현

--- src/main/java/com/sparta/hh2stagram/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/hh2stagram/domain/post/dto/PostResponseDto.java
@@ -1,121 +1,56 @@
 package com.sparta.hh2stagram.domain.post.dto;
 
+import com.sparta.hh2stagram.domain.comment.entity.Comment;
 import com.sparta.hh2stagram.domain.post.entity.Post;
 import com.sparta.hh2stagram.domain.post.entity.PostImage;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PostResponseDto {
 
-    // 게시물 등록
-    @AllArgsConstructor
     @NoArgsConstructor
     @Getter
-    public static class CreatePostResponseDto {
+    public static class PostsResponseDto {
 
-        private Long id;
-
+        private Long postId;
+        private String username; // 게시물 작성한 사람
         private String contents;
-
-        private List<String> imageName;
-        private List<String> imageUrlList;
-        private List<PostImageResponseDto> postImageList = new ArrayList<>();
-
+        private List<PostImageResponseDto> postImageList;
+        private int likes;
+        private boolean like = false;
+        private List<Comment> commentList;
         private LocalDateTime createdAt;
-//        private String formattedTime;
 
-        public CreatePostResponseDto(Post post, List<String> urlList, List<String> nameList) {
-
-
-            this.id = post.getId();
-            this.contents = post.getContents();
-            this.imageName = nameList;
-            this.imageUrlList = urlList;
-            this.createdAt = post.getCreatedAt();
-//            this.formattedTime = formatCreatedAt(post.getCreatedAt());
+        @Builder
+        public PostsResponseDto(Long postId, String username, String contents, List<PostImageResponseDto> postImageList, int likes, boolean like, List<Comment> commentList, LocalDateTime createdAt) {
+            this.postId = postId;
+            this.username = username;
+            this.contents = contents;
+            this.postImageList = postImageList;
+            this.likes = likes;
+            this.like = like;
+            this.commentList = commentList;
+            this.createdAt = createdAt;
         }
-
-        // 생성된 시간을 포맷하는 메서드
-//        private String formatCreatedAt(LocalDateTime createdAt) {
-//            LocalDateTime now = LocalDateTime.now();
-//            long hoursPassed = createdAt.until(now, ChronoUnit.HOURS);
-//
-//            // 시간 간격이 24시간 이하인 경우 시간을 포맷하여 반환
-//            if (hoursPassed <= 24) {
-//                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시간");
-//                return createdAt.format(formatter);
-//            }
-//            // 시간 간격이 24시간 이상인 경우 등록된 년/월/일을 반환
-//            else {
-//                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
-//                return createdAt.format(formatter);
-//            }
-//        }
     }
 
-    // 게시물 수정
-    @AllArgsConstructor
+    // 유저페이지(마이페이지) 조회
     @NoArgsConstructor
     @Getter
-    public static class UpdatePostResponseDto {
-        private Long id;
+    public static class UserPageResponseDto {
 
-        private String contents;
+        private List<PostsResponseDto> posts;
+        private UserInfoResponseDto userInfo;
 
-        private List<String> imageName;
-        private List<String> imageUrlList;
-        private List<PostImageResponseDto> postImageList = new ArrayList<>();
-        private LocalDateTime createdAt;
-        private LocalDateTime modifiedAt;
-
-        public UpdatePostResponseDto(Post post, List<String> urlList, List<String> nameList) {
-            this.id = post.getId();
-
-            this.contents = post.getContents();
-
-            this.imageUrlList = urlList;
-            this.imageName = nameList;
-            this.createdAt = post.getCreatedAt();
-            this.modifiedAt = post.getModifiedAt();
+        @Builder
+        public UserPageResponseDto(List<PostsResponseDto> posts, UserInfoResponseDto userInfo) {
+            this.posts = posts;
+            this.userInfo = userInfo;
         }
-        public UpdatePostResponseDto(Post post) {
-            this.id = post.getId();
-
-            this.contents = post.getContents();
-
-            this.postImageList = post.getPostImageList().stream().map(PostImageResponseDto::new).toList();
-            this.createdAt = post.getCreatedAt();
-            this.modifiedAt = post.getModifiedAt();
-        }
-
-    }
-
-
-    // 게시글 전체 조회
-    @NoArgsConstructor
-    @Getter
-    public static class AllPostResponseDto {
-
-        private Long id;
-        private String contents;
-        private Long userId; // User 엔티티의 id
-//        private List<Long> commentIds; // Comment 엔티티의 id 목록
-        private List<Long> postImageIds; // PostImage 엔티티의 id 목록
-
-        public AllPostResponseDto(Post post) {
-            this.id = post.getId();
-            this.contents = post.getContents();
-            this.userId = post.getUser().getId(); // User 엔티티의 id 가져오기
-//            this.commentIds = post.getCommentList().stream().map(Comment::getId).collect(Collectors.toList()); // Comment 엔티티의 id 목록 가져오기
-            this.postImageIds = post.getPostImageList().stream().map(PostImage::getId).collect(Collectors.toList()); // PostImage 엔티티의 id 목록 가져오기
-        }
-
     }
 
     @NoArgsConstructor
@@ -129,6 +64,21 @@ public class PostResponseDto {
             this.id = postImage.getId();
             this.url = postImage.getUrl();
             this.imageName = postImage.getImageName();
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class UserInfoResponseDto {
+        private int postNumber;
+        private int follower;
+        private int following;
+
+        @Builder
+        public UserInfoResponseDto(int postNumber, int follower, int following) {
+            this.postNumber = postNumber;
+            this.follower = follower;
+            this.following = following;
         }
     }
 }
