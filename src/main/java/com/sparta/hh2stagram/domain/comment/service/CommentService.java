84b2147c573e751e7134d0bd6bb6860824c07d5c
sanha <0412csh@gmail.com>
2024-03-22 12:23:05 +0900

    ✅ Add: 댓글 createAt & formattedTime 추가

--- src/main/java/com/sparta/hh2stagram/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/hh2stagram/domain/comment/service/CommentService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,6 +43,7 @@ public class CommentService {
                 .commentId(comment.getId())
                 .username(user.getUsername())
                 .content(comment.getContent())
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 
@@ -62,6 +64,7 @@ public class CommentService {
                         .commentId(comment.getId())
                         .username(comment.getUser().getUsername())
                         .content(comment.getContent())
+                        .createdAt(LocalDateTime.now())
                         .build())
                 .collect(Collectors.toList());
 
