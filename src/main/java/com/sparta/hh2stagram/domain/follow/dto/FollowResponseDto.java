8783bcfa72a774ff91d316d4d313935b5e60b40f
Seungmi97 <mi.seung97@gmail.com>
2024-03-23 04:13:32 +0900

    Fix: 팔로잉, 팔로워 response dto에 follow id 추가

--- src/main/java/com/sparta/hh2stagram/domain/follow/dto/FollowResponseDto.java
+++ src/main/java/com/sparta/hh2stagram/domain/follow/dto/FollowResponseDto.java
@@ -7,10 +7,12 @@ public class FollowResponseDto {
 
     @Getter
     public static class FollowerResponseDto {
+        private final Long id;
         private final Long followerId;
         private final String followerUsername;
 
-        public FollowerResponseDto(Long followerId, String followerUsername) {
+        public FollowerResponseDto(Long id, Long followerId, String followerUsername) {
+            this.id = id;
             this.followerId = followerId;
             this.followerUsername = followerUsername;
         }
@@ -18,10 +20,12 @@ public class FollowResponseDto {
 
     @Getter
     public static class FollowingResponseDto {
+        private final Long id;
         private final Long followingId;
         private final String followingUsername;
 
-        public FollowingResponseDto(Long followingId, String followingUsername) {
+        public FollowingResponseDto(Long id, Long followingId, String followingUsername) {
+            this.id = id;
             this.followingId = followingId;
             this.followingUsername = followingUsername;
         }
